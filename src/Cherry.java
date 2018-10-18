c09d02c419b615d72018745ed3dff4d19ebd14ec
Michael Sedov <sedovmikh@yandex.ru>
2018-10-18 19:17:29 +0300

    Declare Cherry class and an abstract class - Fruit

---
+++ src/Cherry.java
@@ -0,0 +1,8 @@
+import java.awt.*;
+public class Cherry extends Fruit {
+
+
+    public void draw(Graphics g) {
+
+    }
+}
---
+++ src/Fruit.java
@@ -0,0 +1,5 @@
+import java.awt.*;
+
+public abstract class Fruit {
+    abstract void draw(Graphics g);
+}
