ba3785261a7130a43e4b05347cc33521570ae29f
Michael Sedov <sedovmikh@yandex.ru>
2018-10-18 13:23:36 +0300

    Implement KeyListener

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -2,8 +2,10 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
-public class GamePanel extends JPanel implements ActionListener {
+public class GamePanel extends JPanel implements ActionListener, KeyListener {
 
     private Snake snake = new Snake();
     private Timer snakeTimer;
@@ -18,6 +20,34 @@ public class GamePanel extends JPanel implements ActionListener {
     public void actionPerformed(ActionEvent e) {
         snake.update();
         repaint();
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        switch(e.getKeyCode()) {
+            case KeyEvent.VK_W:
+                snake.setSnakeDirection(SnakeDirection.UP);
+                break;
+            case KeyEvent.VK_S:
+                snake.setSnakeDirection(SnakeDirection.DOWN);
+                break;
+            case KeyEvent.VK_A:
+                snake.setSnakeDirection(SnakeDirection.LEFT);
+                break;
+            case KeyEvent.VK_D:
+                snake.setSnakeDirection(SnakeDirection.RIGHT);
+                break;
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
     }
 
     @Override
